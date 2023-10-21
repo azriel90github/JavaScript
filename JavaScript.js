d6f884681e44123a6acdf2ab700a968f825d5adb
azriel90github <azrielgithub@gmail.com>
2023-10-21 14:43:04 +0100

    03

---
+++ JavaScript.js
@@ -0,0 +1,13 @@
+//Dispara de Janelas - Primeira Aula
+
+//alert('Primero Programa - Mostrar Contéudo');
+
+//confirm('Segundo Programa - Ok ou cancelar');
+
+//prompt('Terceiro Programa - Inserir Texto');
+
+//Fim da Primeira Aula
+
+
+//Segunda Aula
+
