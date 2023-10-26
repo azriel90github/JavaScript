5f10599525107cd0e0550bd16d85746aa9b45b67
azriel90github <azrielgithub@gmail.com>
2023-10-26 22:24:54 +0100

    07

---
+++ Condições/ex001.js
@@ -0,0 +1,16 @@
+//console.log('O console funcionou corretamente!')
+var velocidade = 61
+//Mostrar na tela a variavel velocidade
+console.log(`A velocidade do carro é de ${velocidade}km/h`)
+
+//Condição Simples(if), se a Velocidade for maior (>) que 60, deve mostrar dentro do bloco
+if (velocidade > 60) {
+    console.log(`Você ultrapassou  a velossidade permitida. MULTADO!`)
+} 
+
+/*else {
+    console.log('Você ultrapassou o limite da velocidade, foste Multado!')
+}
+*/
+
+console.log('Diriga sempre usando o cinto de Segurança')
---
+++ Condições/ex002.js
@@ -0,0 +1,14 @@
+//variavel
+var pais = 'França'
+
+//mostrar na tela a variavel pais
+console.log(`Vivendo em ${pais}`)
+
+//se for igual a brasil, você é brasileiro
+if(pais == 'Brasil') {
+    console.log('Você é Brasileiro')
+} 
+//se não você é estrangeiro
+else {
+    console.log('Você é estrangeiro')
+}
