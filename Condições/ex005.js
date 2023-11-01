b94142868a770b59b6b873ff0d4fa849b18e17a3
azriel90github <azrielgithub@gmail.com>
2023-11-01 19:26:20 +0000

    05

--- Condições/ex005.js
+++ Condições/ex005.js
@@ -1,4 +1,55 @@
 var data = new Date()
 var diaSem = data.getDay()
 
-console.log(diaSem)
+var diaSem = 5
+
+/*
+
+0 = Domingo
+1 = Segunda
+2 = Terça
+3 = Quarta
+4 = Quinta
+5 = Sexta
+6 = Sabado
+
+*/
+
+//console.log(diaSem)
+
+//expressão switch para a var diaSem
+switch(diaSem) {
+    //se a caixa ter o valor 0 mostrar na tela Domingo
+    //Nota: sem esquecer o break para dar um fim na execução de caixa
+    case 0: 
+        console.log('Domingo')
+        break
+
+    case 1: 
+        console.log('Segunda')
+        break
+
+    case 2:
+        console.log('Terça')
+        break
+
+    case 3: 
+        console.log('Quarta')
+        break
+
+    case 4: 
+        console.log('Quinta')
+        break
+
+    case 5: 
+        console.log('Sexta')
+        break
+
+    case 6: 
+        console.log('Sabado')
+        break
+
+    default: 
+        console.log('[ERRO] Dia Inválido!')
+    break
+} 
