b3a00d44d0b85ec1582e3ba04e516762b2f64744
azriel90github <azrielgithub@gmail.com>
2023-11-02 18:32:44 +0000

    16

---
+++ exercicios/ex002/ex002.js
@@ -0,0 +1,14 @@
+
+function verificar() {
+
+    //alert('Funcionou')
+
+    var data = new Date()
+    var ano = data.getFullYear()
+    var nascimento = document.querySelector('input#ano')
+    var resultado = document.querySelector('div#resultado')
+    
+    // resultado.innerHTML = 
+    //resultado.document.write = `Sua idade é ${nascimento}`
+
+}
