96b43f95cf1cb19a8fd29bdbb5c50025290abd61
azriel90github <azrielgithub@gmail.com>
2023-11-04 20:48:42 +0000

    01

--- exercicios/ex002/ex002.js
+++ exercicios/ex002/ex002.js
@@ -1,16 +1,21 @@
 //funcao verificar
 function verificar() {
 
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        alert('[ERROR]Verifique os dados e tente novamente!')
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fano = document.getElementById('txtano');
+    var res = document.querySelector('div#res');
+    
+    if (fano.value.length == 0 || fano.value > ano) {
+
+        alert('[ERROR]Verifique os dados e tente novamente!');
+
     } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade Calculada: ${idade}`
+
+        var fsex = document.getElementsByName('radsex');
+        var idade = ano - Number(fano.value);
+        res.innerHTML = `Idade Calculada: ${idade}`;
+
     }
 
 }
