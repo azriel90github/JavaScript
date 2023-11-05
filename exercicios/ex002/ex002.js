e750685ccfcd142d3adc2ae0f0c67c91fc8af581
azriel90github <azrielgithub@gmail.com>
2023-11-05 20:55:40 +0000

    01

--- exercicios/ex002/ex002.js
+++ exercicios/ex002/ex002.js
@@ -14,8 +14,35 @@ function verificar() {
 
         var fsex = document.getElementsByName('radsex');
         var idade = ano - Number(fano.value);
-        res.innerHTML = `Idade Calculada: ${idade}`;
+        //res.innerHTML = `Idade Calculada: ${idade}`;
+        var genero =  ''
+        //variavel img
+        var img = document.createElement('img')
+        //tag img com o id foto, dentro do JS
+        img.setAttribute('id', 'foto')
 
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade >10) {
+                //criança
+
+            } else if (idade < 21) {
+                //jovem
+            } else if (idade < 50) {
+                //idoso
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade >10) {
+                //criança
+
+            } else if (idade < 21) {
+                //jovem
+            } else if (idade < 50) {
+                //idoso
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
     }
 
 }
