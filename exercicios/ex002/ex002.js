5e44790c57c8bfae2507ff228c58e178175a9b5d
azriel90github <azrielgithub@gmail.com>
2023-11-06 10:40:04 +0000

    17

--- exercicios/ex002/ex002.js
+++ exercicios/ex002/ex002.js
@@ -25,21 +25,37 @@ function verificar() {
             genero = 'Homem'
             if (idade >= 0 && idade >10) {
                 //criança
+                img.setAttribute('src', 'foto-bebe-homem.jpg')
 
             } else if (idade < 21) {
                 //jovem
+                img.setAttribute('src', 'foto-jovem-homem.jpg')
+
             } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'foto-adulto-homem.jpg')
+            } else {
                 //idoso
+                img.setAttribute('src', 'foto-idoso-homem.jpg')
             }
+
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >= 0 && idade >10) {
                 //criança
+                img.setAttribute('src', 'foto-bebe-mulher')
 
             } else if (idade < 21) {
                 //jovem
+                img.setAttribute('src', 'foto-jovem-mulher')
+
             } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'foto')
+
+            } else {
                 //idoso
+                img.setAttribute('src', 'caminho da foto')
             }
         }
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
