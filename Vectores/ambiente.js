4dd287f58628d45d5294807c6a999e2f940810bb
azriel90github <azrielgithub@gmail.com>
2023-12-15 21:20:16 +0000

    01

--- Vectores/ambiente.js
+++ Vectores/ambiente.js
@@ -8,7 +8,7 @@ num.push(1)// Definir como último número na Ordem
 
 console.log(num)
 console.log(`O Vector têm ${num.length} posições`)// Número de Valores dentro do vector
-console.log(`O primeiro valor do vector é ${num[1]}`)// Buscar um valor no Vector 1
+console.log(`O primeiro valor do vector é ${num[1]}`)// Buscar um valor no Vector 1 
 
 
 /*
