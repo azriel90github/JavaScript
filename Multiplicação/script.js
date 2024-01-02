e77a00eb759e646974fdec46ae9114c4210ef6e5
azriel90github <azrielgithub@gmail.com>
2024-01-02 21:34:37 +0000

    01

--- Multiplicação/script.js
+++ Multiplicação/script.js
@@ -2,29 +2,30 @@
 
 function tabuada() {
 
-    let num = document.getElementById('txtn')
-    let tab = document.getElementById('seltab')
+    let num = document.getElementById('txtn') // variavel num, bucando o ID txtn
+    let tab = document.getElementById('seltab') // variavel tab buscando o ID seltab
 
 
-    if (num.value.length == 0) {
+    if (num.value.length == 0) { // Se o valor do num for igual a 0
         alert('Por favor digite um número!')
     } 
     
     
-    else {
+    else { // Se não
 
-        let n = Number(num.value)
-        let c =1
+        let n = Number(num.value) // Variavel n recebe Valor do número
+        let c = 1 // Variavel c recebe valor 1l
 
         tab.innerHTML = ''
 
-        while (c <= 10) {
+        while (c <= 10) { // Se c for menor ou igual 10
 
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
-            item.value = `tab${c}`
-            tab.appendChild(item)
-            c++
+            let item = document.createElement('option') // Variavel item, criando o elemento option
+            item.text = `${n} x ${c} = ${n*c}` // Item recebe o texto...
+            item.value = `tab${c}` // Valor do item
+            tab.appendChild(item) // Anexar o item
+
+            c++ // Incrementar
 
         }
     }
