86e6bf1f0ec3ba7212e2f1ef6b39baae763501a3
azriel90github <azrielgithub@gmail.com>
2024-01-07 20:53:16 +0000

    01

---
+++ w3c.js
@@ -0,0 +1,77 @@
+
+//let firstName = "John", lastName = "Doe", age = 35;
+
+
+/*
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// You can change an element:
+cars[0] = "Toyota";
+
+// You can add an element:
+cars.push("Audi");
+*/
+
+
+
+// You can create a const object:
+/*
+const car = {
+
+    type:"Fiat", model:"500", color:"white"
+    
+
+};
+
+console.log(car.type)
+
+// You can change a property:
+
+car.color = "red";
+console.log(car.color)
+
+
+
+// You can add a property:
+car.owner = "Johnson";
+console.log(car.owner)
+*/
+//----------------------------------------------------------------------
+
+
+//Tipos de Dados
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+/*
+const person = {firstName:"John", lastName:"Doe"};
+*/
+// Array object - Matriz:
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars[1])
+
+// Date object:
+const date = new Date("2022-03-25");
+
+
+
+const person = {
+    
+    firstName:"John", 
+    lastName:"Doe", 
+    age:50, 
+    eyeColor:"blue"
+};
+
+console.log(person.age)
